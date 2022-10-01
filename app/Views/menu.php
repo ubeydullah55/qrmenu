143970db6814ba0b071d6ce32f72e4cdff5e771b
HISARHOSPITAL\ubeydullah.dogan <ubeydullah.dogan@hisarhospital.com>
2022-10-01 17:08:06 +0300

    deneme

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -56,7 +56,7 @@
         <img style="max-width:100%;height: auto;" src="<?= base_url('assets/frontend/images/logo1.png'); ?>" alt="logo_resim" srcset="">
         </div>
         <h2>
-         Lezzet Lokantasıı
+         Lezzet Lokantası
         </h2>
         <br>
      <?php $table_no=$_GET["id"]; ?>
